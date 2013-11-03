53ea1956a48ad45834d702deac8ce9d6e9e5237d
Jaroslav Vitku <jarda.vitku@gmail.com>
2013-11-03 12:45:32 +0100

    changed links

--- simulator-ui/nr-demo/gates/logic_crisp_gates.py
+++ simulator-ui/nr-demo/gates/logic_crisp_gates.py
@@ -1,1 +1,74 @@
-/Users/j/Dropbox/workspace/nengoros/demonodes/../logic/gates//python/nr-demo/logic_crisp_gates.py
+# Demo showing how to use crisp logic gates implemented as ROS nodes in the Nengoros.
+#
+#
+# To launch this script: open the Nengo gui (probably nengo/simulator-ui/nengo) and weite into the command line:
+# 	run nr-demo/gates/logic_crisp_gates.py
+# Note that this cript has to be either symlinked or copied into the location: nengo/simulator-ui/nengo/nr-demo/gates/
+#
+# by Jaroslav Vitku [vitkujar@fel.cvut.cz]
+
+import nef
+from ca.nengo.math.impl import FourierFunction
+from ca.nengo.model.impl import FunctionInput
+from ca.nengo.model import Units
+from rosnodes import logic_gates
+
+# creates nef network and adds it to nengo (this must be first in the script) 
+net=nef.Network('Demo script which shows network of logic gates')
+net.add_to_nengo()  # here: delete old (toplevel) network and replace it with the newly CREATED one
+
+# Create function generators, white noise with params: baseFreq, maxFreq [rad/s], RMS, seed
+gen1=FunctionInput('Randomized input 1', [FourierFunction(.1, 10,3, 12)],Units.UNK) 
+gen2=FunctionInput('Randomized input 2', [FourierFunction(.3, 30,2, 2)],Units.UNK) 
+net.add(gen1)
+net.add(gen2)
+
+# Add nodes
+myAND = logic_gates.and_node("myAND")
+net.add(myAND)
+
+myXOR = logic_gates.xor_node("myXOR")
+net.add(myXOR)
+
+myNAND = logic_gates.nand_node("myNAND")
+net.add(myNAND)
+
+myOR = logic_gates.or_node("myOR")
+net.add(myOR)
+
+myNOT = logic_gates.not_node("myNOT")
+net.add(myNOT)
+
+# Wire inputs
+net.connect(gen1, myAND.getTermination('logic/gates/ina'))
+net.connect(gen2, myAND.getTermination('logic/gates/inb'))
+
+net.connect(gen1, myXOR.getTermination('logic/gates/ina'))
+net.connect(gen2, myXOR.getTermination('logic/gates/inb'))
+
+net.connect(gen1, myNAND.getTermination('logic/gates/ina'))
+net.connect(gen2, myNAND.getTermination('logic/gates/inb'))
+
+net.connect(gen1, myOR.getTermination('logic/gates/ina'))
+net.connect(gen2, myOR.getTermination('logic/gates/inb'))
+
+net.connect(gen1, myNOT.getTermination('logic/gates/ina'))
+
+# Wire outputs
+A=net.make('AND_net',neurons=100,dimensions=1,radius=1)    
+net.connect(myAND.getOrigin('logic/gates/outa'), A)
+
+B=net.make('XOR_net',neurons=100,dimensions=1,radius=1)    
+net.connect(myXOR.getOrigin('logic/gates/outa'), B)
+
+C=net.make('NAND_net',neurons=100,dimensions=1,radius=1)    
+net.connect(myNAND.getOrigin('logic/gates/outa'), C)
+
+D=net.make('OR_net',neurons=100,dimensions=1,radius=1)    
+net.connect(myOR.getOrigin('logic/gates/outa'), D)
+
+E=net.make('NOT_net',neurons=100,dimensions=1,radius=1)    
+net.connect(myNOT.getOrigin('logic/gates/outa'), E)
+
+
+print 'Configuration complete.'
--- simulator-ui/python/nef/templates/logic_fuzzy_member_linearInc.py
+++ simulator-ui/python/nef/templates/logic_fuzzy_member_linearInc.py
@@ -1,1 +1,55 @@
-/Users/j/Dropbox/workspace/nengoros/demonodes/../logic/gates//python/nef/templates/logic_fuzzy_member_linearInc.py
+# Drag and drop template for the Nengo simulator which represents ROS node implementing particular fuzzy membership function.
+#
+# by Jaroslav Vitku
+#
+# for more information how to make such template, see: http://nengo.ca/docs/html/advanced/dragndrop.html or notes/add_node_to_gui.md
+#
+
+import nef
+import math
+from ca.nengo.math.impl import FourierFunction
+from ca.nengo.model.impl import FunctionInput
+from ca.nengo.model import Units
+#from ctu.nengoros.modules.impl import DefaultNeuralModule as NeuralModule
+from ctu.nengoros.modules.impl import DefaultAsynNeuralModule as NeuralModule
+from ctu.nengoros.comm.nodeFactory import NodeGroup as NodeGroup
+from ctu.nengoros.comm.rosutils import RosUtils as RosUtils
+
+# node utils..
+title='FuzzyMemLinInc'
+label='FuzzyMemLinInc'
+icon='logic_fuzzy_member_linearInc.png'
+
+# parameters for initializing the node
+params=[
+('name','Select name for NeuralModule Fuzzy Membership function linear increasing',str),
+('independent','Can be group pndependent? (pushed into namespace?) select true',bool)
+]
+
+# try to instantiate node with given parameters (e.g. check name..)
+def test_params(net,p):
+    try:
+       net.network.getNode(p['name'])
+       return 'That name is already taken'
+    except:
+        pass
+
+
+def make(net,name='NeuralModule which implements FuzzyMembership function - Increasing Linear', 
+independent=True, useQuick=True):
+
+    finder = "org.hanns.logic.fuzzy.membership.impl.IncreasingLinear";
+
+    # create group with a name
+    g = NodeGroup(name, independent);   
+    g.addNode(finder, "FuzzyMemLinInc", "java");     
+
+    neuron = NeuralModule(name+"_FuzzyMemLinInc", g) 
+    neuron.createEncoder("logic/gates/ina", "float",1)   	# termination = data input x 
+    neuron.createEncoder("logic/gates/confa", "float",1)	# termination - config input alpha
+    neuron.createEncoder("logic/gates/confb", "float",1)	# termination - config input beta
+    neuron.createDecoder("logic/gates/outa", "float",1)  	# origin = output of neuron = data output y
+
+
+    many=net.add(neuron)                    # add it into the network
+
