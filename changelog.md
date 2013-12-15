c4f92e51825161108c5b815966d5e35b6dbf4ba9
Jaroslav Vitku <jarda.vitku@gmail.com>
2013-12-15 18:03:09 +0100

    added changelog file

---
+++ changelog.md
@@ -0,0 +1,19 @@
+Tracks changes in the original Nengo simulator
+======================================
+
+# nengoros-master-v0.0.1
+
+First official verison of Nengoros simulator. 
+
+# nengoros-master-v0.0.2
+
+Adds support of time synchronization with the ROS network:
+	-TimeMaster
+	-TimeIgnore
+	-TimeSlave (experimental)
+	
+# nengoros-master-v0.0.3
+
+Adds support of passing private parameters to nodes (includes jroscore project) from the commandline or ParameterServer. 
+Each node can read own private parameters by means of PrivateRosparam class.
+
--- simulator-ui/scripts/w1.py
+++ simulator-ui/scripts/w1.py
@@ -1,8 +1,4 @@
-# Create the NeuralModule which receives 4 float values, finds min and max, converts them to int and passes to the output.
-#
-# starts: 
-#   -ROS-java node (class extending the org.ros.Node) which does exactly the thing described above
-#   -NeuralModule with modem that communicates with the ROS node 
+# Test weighted connections for purposes of neuroevolution
 #
 # by Jaroslav Vitku [vitkujar@fel.cvut.cz]
 
@@ -11,6 +7,26 @@ from ca.nengo.math.impl import FourierFunction
 from ca.nengo.model.impl import FunctionInput
 from ca.nengo.model import Units
 from rosnodes import logic_gates
+
+from ctu.nengoros.comm.nodeFactory import NodeGroup as NodeGroup
+from ctu.nengoros.comm.rosutils import RosUtils as RosUtils
+from ctu.nengoros.modules.impl import DefaultNeuralModule as NeuralModule
+
+AND 		= "org.hanns.logic.crisp.gates.impl.AND";
+XOR 		= "org.hanns.logic.crisp.gates.impl.XOR";
+OR 			= "org.hanns.logic.crisp.gates.impl.OR";
+NAND 		= "org.hanns.logic.crisp.gates.impl.NAND";
+NOT 		= "org.hanns.logic.crisp.gates.impl.NOT";
+
+# Add nodes
+def or_node(name):
+	g = NodeGroup("OR", True);
+	g.addNode(OR, "OR", "java");
+	module = NeuralModule(name+'_OR', g)
+	module.createEncoder("logic/gates/ina", "bool", 1)
+	module.createEncoder("logic/gates/inb", "bool", 1)
+	module.createDecoder("logic/gates/outa", "bool", 1)
+	return module
 
 # creates nef network and adds it to nengo (this must be first in the script) 
 net=nef.Network('Weighted connections test 1')
@@ -22,18 +38,18 @@ gen2=FunctionInput('Randomized input 2', [FourierFunction(.3, 30,2, 2)],Units.UN
 net.add(gen1)
 net.add(gen2)
 
-# Add nodes
-myAND = logic_gates.and_node("myAND")
-net.add(myAND)
+
+myOR=or_node("myOR")	
+net.add(myOR)
 
 myNOT = logic_gates.not_node("myNOT")
 net.add(myNOT)
 
 # Wire inputs
-net.connect(gen1, myAND.getTermination('logic/gates/ina'))
-net.connect(gen2, myAND.getTermination('logic/gates/inb'))
+net.connect(gen1, myOR.getTermination('logic/gates/ina'))
+net.connect(gen2, myOR.getTermination('logic/gates/inb'))
 
-net.connect(myAND.getOrigin('logic/gates/outa'), myNOT.getTermination('logic/gates/ina'), weight=1)
+net.connect(myOR.getOrigin('logic/gates/outa'), myNOT.getTermination('logic/gates/ina'), weight=1)
 
 
 #net.connect(gen1, myNOT.getTermination('logic/gates/ina'))
