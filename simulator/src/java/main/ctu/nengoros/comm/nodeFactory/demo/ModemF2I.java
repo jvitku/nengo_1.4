d5754309b98ed0d787009c7bca5ab745e83e3ae0
Jaroslav Vitku <jarda.vitku@gmail.com>
2014-01-30 14:24:06 +0100

    just a warning

--- simulator/src/java/main/ctu/nengoros/comm/nodeFactory/demo/ModemF2I.java
+++ simulator/src/java/main/ctu/nengoros/comm/nodeFactory/demo/ModemF2I.java
@@ -4,9 +4,7 @@ import ctu.nengoros.comm.nodeFactory.NodeFactory;
 import ctu.nengoros.comm.nodeFactory.NodeGroup;
 import ctu.nengoros.comm.rosutils.Mess;
 import ctu.nengoros.comm.rosutils.RosUtils;
-import ctu.nengoros.exceptions.ConnectionException;
 import ctu.nengoros.modules.NeuralModule;
-//import ctu.nengoros.modules.test.NeuralModuleTest;
 import ctu.nengoros.modules.impl.DefaultNeuralModule;
 
 /**
