ec5fc70382c4dab4224cfd6f0392299a77e58926
Jaroslav Vitku <jarda.vitku@gmail.com>
2015-04-25 11:55:25 +0200

    ...

--- simulator/src/java/main/ctu/nengorosHeadless/simulator/test/QLambdaTest.java
+++ simulator/src/java/main/ctu/nengorosHeadless/simulator/test/QLambdaTest.java
@@ -93,7 +93,7 @@ public class QLambdaTest{
 		System.out.println("all done, reset, waiting");
 		sim.reset(false);
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(200000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
